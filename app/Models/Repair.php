4203a756cd88858cdb7977b4a76e3da4338a1d31
Ahsan AUA <61789531+AUAboi@users.noreply.github.com>
2023-02-13 17:47:53 +0500

    home design

--- app/Models/Repair.php
+++ app/Models/Repair.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent
 class Repair extends Model
 {
     use HasFactory;
