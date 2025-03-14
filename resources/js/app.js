3229503b0b07528d6491fb60d5d63325d64abf93
Ahsan Faisal <auawgamers@gmail.com>
2025-03-14 12:58:30 +0500

    added header and banner

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import './bootstrap';
-import '../css/app.scss';
-import "../css/unreset.scss"
+import "./bootstrap";
+import "../css/style.css";
+import "../css/unreset.scss";
 // import "@fontsource/jost/300.css"
 // import "@fontsource/jost/400.css"
 // import "@fontsource/jost/500.css"
@@ -9,37 +9,40 @@ import "../css/unreset.scss"
 // import "@fontsource/jost/800.css"
 // import "@fontsource/jost/variable.css";
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3'
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createPinia } from 'pinia'
-import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createPinia } from "pinia";
+import { ZiggyVue } from "../../vendor/tightenco/ziggy/dist/vue.m";
 
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue"
-import PublicLayout from "@/Layouts/PublicLayout.vue"
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import PublicLayout from "@/Layouts/PublicLayout.vue";
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName =
+  window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
 
-const pinia = createPinia()
+const pinia = createPinia();
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: async (name) => {
-        const page = await resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue'))
-        if (name.startsWith('Admin')) page.default.layout = AuthenticatedLayout
-        if (name.startsWith('Public')) page.default.layout = PublicLayout
-        return page;
-    },
-    progress: {
-        showSpinner: true,
-        color: "#dc2626"
-    },
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .use(pinia)
-            .mount(el);
-    },
+  title: (title) => `${title} - ${appName}`,
+  resolve: async (name) => {
+    const page = await resolvePageComponent(
+      `./Pages/${name}.vue`,
+      import.meta.glob("./Pages/**/*.vue")
+    );
+    if (name.startsWith("Admin")) page.default.layout = AuthenticatedLayout;
+    if (name.startsWith("Public")) page.default.layout = PublicLayout;
+    return page;
+  },
+  progress: {
+    showSpinner: true,
+    color: "#dc2626",
+  },
+  setup({ el, App, props, plugin }) {
+    return createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .use(ZiggyVue, Ziggy)
+      .use(pinia)
+      .mount(el);
+  },
 });
-
