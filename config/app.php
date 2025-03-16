ec18153e8ce49c62fdf72b6076adeafeab1a10a1
Ahsan Faisal <auawgamers@gmail.com>
2025-03-16 12:58:13 +0500

    ttest

--- config/app.php
+++ config/app.php
@@ -185,7 +185,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Darryldecode\Cart\CartServiceProvider::class,
+        // Darryldecode\Cart\CartServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -208,8 +208,8 @@ return [
     |
     */
 
-    'aliases' => Facade::defaultAliases()->merge([
-        'Cart' => Darryldecode\Cart\Facades\CartFacade::class
-    ])->toArray(),
+    // 'aliases' => Facade::defaultAliases()->merge([
+    //     'Cart' => Darryldecode\Cart\Facades\CartFacade::class
+    // ])->toArray(),
 
 ];
