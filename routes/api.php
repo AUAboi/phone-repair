5610d91952b7a5762004a25338a09ab42ad9eb3d
Ahsan Faisal <auawgamers@gmail.com>
2025-03-18 19:11:13 +0500

    gbp

--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::post('payment-intent', function (Request $request) {
     try {
         $paymentIntent = PaymentIntent::create([
             'amount' => $request->amount, // Amount in cents (e.g., 3500 for €35)
-            'currency' => 'eur', // Klarna supports EUR, USD, GBP, etc.
+            'currency' => 'gbp', // Klarna supports EUR, USD, GBP, etc.
             'payment_method_types' => ['card', 'link', 'klarna', 'paypal'], // Include Klarna
         ]);
 
